6191b0a18ebe7f062eb50437be420bd9c738eead
barcochrist <barcochrist@gmail.com>
2018-12-16 12:50:13 -0500

    Refactor method to send email with Gmail

--- app/src/main/java/com/programacionmaster/sendemail/MainActivity.java
+++ app/src/main/java/com/programacionmaster/sendemail/MainActivity.java
@@ -1,13 +1,141 @@
 package com.programacionmaster.sendemail;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextTo, editTextSubject, editTextMessage;
+    private Button buttonSendEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextTo = findViewById(R.id.editTextTo);
+        editTextSubject = findViewById(R.id.editTextSubject);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        buttonSendEmail = findViewById(R.id.buttonSendEmail);
+
+        //TODO Define recipient information
+        final String recipientEmail = "";
+        final String recipientPassword = "";
+
+        buttonSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmailWithGmail(
+                        recipientEmail,
+                        recipientPassword,
+                        editTextTo.getText().toString(),
+                        editTextSubject.getText().toString(),
+                        editTextMessage.getText().toString());
+            }
+        });
+    }
+
+    /**
+     * Send email with Gmail service.
+     *
+     * @param recipient
+     * @param recipientPassword
+     * @param to
+     * @param subject
+     * @param message
+     */
+    private void sendEmailWithGmail(final String recipient, final String recipientPassword, String to,
+                                    String subject, String message) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(recipient, recipientPassword);
+            }
+        });
+
+        SenderAsyncTask task = new SenderAsyncTask(session, recipient, to, subject, message);
+        task.execute();
+    }
+
+    /**
+     * AsyncTask to send email
+     */
+    private class SenderAsyncTask extends AsyncTask<String, String, String> {
+
+        private String from, to, subject, message;
+        private ProgressDialog progressDialog;
+        private Session session;
+
+        public SenderAsyncTask(Session session, String from, String to, String subject, String message) {
+            this.session = session;
+            this.from = from;
+            this.to = to;
+            this.subject = subject;
+            this.message = message;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(MainActivity.this, "", getString(R.string.sending_mail), true);
+            progressDialog.setCancelable(false);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(from));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+                message.setSubject(subject);
+                message.setContent(message, "text/html; charset=utf-8");
+                Transport.send(message);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+                return e.getMessage();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return e.getMessage();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            progressDialog.setMessage(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            progressDialog.dismiss();
+            editTextTo.setText("");
+            editTextSubject.setText("");
+            editTextMessage.setText("");
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+        }
     }
 }
