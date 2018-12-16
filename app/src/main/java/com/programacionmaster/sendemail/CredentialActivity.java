0ea1bce3c549a34e604d18c5e4fdb06a900e5627
barcochrist <barcochrist@gmail.com>
2018-12-16 12:55:58 -0500

    Create Credentials activity

---
+++ app/src/main/java/com/programacionmaster/sendemail/CredentialActivity.java
@@ -0,0 +1,13 @@
+package com.programacionmaster.sendemail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CredentialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credential);
+    }
+}
